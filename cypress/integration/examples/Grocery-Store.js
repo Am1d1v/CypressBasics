77bd8828d0360a296416a0a1ef868066e8be83eb
Am1d1v <dimadiman1adima@gmail.com>
2024-06-28 13:13:26 +0300

    display message that the searching product does not exist

--- cypress/integration/examples/Grocery-Store.js
+++ cypress/integration/examples/Grocery-Store.js
@@ -7,7 +7,7 @@ describe('Grocery Store', () => {
         cy.get('.stepper-input .quantity').should('have.value', 1);
     });
 
-    it.only('should find "Brocolli" in search input field', () => {
+    it('should find "Brocolli" in search input field', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
 
         // Type "Brocolli" in search input field
@@ -17,5 +17,14 @@ describe('Grocery Store', () => {
         cy.get('.products > .product').should('exist');
     });
 
+    it.only('should display message that the searching product does not exist', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+
+        // Type "Nothing" in search input field
+        cy.get('.search-keyword').type('Nothing');
+
+        // Product doesn't exist
+        cy.get('.no-results > h2').contains('Sorry, no products matched your search!');
+    });
 
 });
