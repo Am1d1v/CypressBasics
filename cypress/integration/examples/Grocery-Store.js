f441d706f10e79c51a41bfc8e0640f971b3d26a4
Am1d1v <dimadiman1adima@gmail.com>
2024-06-28 12:42:38 +0300

    Search single product in grocery store

--- cypress/integration/examples/Grocery-Store.js
+++ cypress/integration/examples/Grocery-Store.js
@@ -2,9 +2,20 @@
 
 describe('Grocery Store', () => {
 
-    it('Default number of products is 1', () => {
+    it('should check that default number of products is 1', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
         cy.get('.stepper-input .quantity').should('have.value', 1);
-    })
+    });
+
+    it.only('should find "Brocolli" in search input field', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+
+        // Type "Brocolli" in search input field
+        cy.get('.search-keyword').type('Brocolli');
+
+        // Check that product exists
+        cy.get('.products > .product').should('exist');
+    });
+
 
 });
