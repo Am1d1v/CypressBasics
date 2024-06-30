d2d8c6622f0ea6601771087c247904c47c50e6f9
Am1d1v <dimadiman1adima@gmail.com>
2024-06-30 14:44:59 +0300

    UI Controls. Should uncheck certain check boxes

--- cypress/integration/examples/UI-Controls.js
+++ cypress/integration/examples/UI-Controls.js
@@ -10,4 +10,23 @@ describe('UI Controls', () => {
         cy.get('#checkBoxOption3').check().should('be.checked').and('have.value', 'option3');;
     });
 
-})
+    it.only('should uncheck certain check boxes', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice');
+
+        // Select 1 and 3 check boxes
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
+        cy.get('#checkBoxOption3').check().should('be.checked').and('have.value', 'option3');;
+
+        // Uncheck 1 and 3 check boxes
+        cy.get('#checkBoxOption1').uncheck();
+        cy.get('#checkBoxOption3').uncheck();
+
+        // 1 and 3 check boxes must have be unchecked
+        cy.get('#checkBoxOption1').check().should('not.be.checked');
+        cy.get('#checkBoxOption3').check().should('not.be.checked');
+
+    })
+
+
+
+});
