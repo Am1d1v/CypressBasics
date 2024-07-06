d1ba6279b3d7ed132d4c1480e0b3d3014bf83dea
Am1d1v <dimadiman1adima@gmail.com>
2024-07-06 14:16:19 +0300

    Table Handle. Course mathes with course's price

--- cypress/integration/examples/Table-Handle.js
+++ cypress/integration/examples/Table-Handle.js
@@ -2,15 +2,17 @@
 
 describe('Table Handle', () => {
 
-    it.only("should validate course and it's price", () => {
+    it.only("should validate 'SQL' course and it's price", () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice');
 
-        cy.get('tr td:nth-child(2)').each(item => {
+        cy.get('tr td:nth-child(2)').each((item, index) => {
             const text = item.text()
             
-            if(text.includes('SQL')){
+            if(text.includes('Learn SQL')){
                 // Get "Price" column
-                cy.get('tr td:nth-child(2)').next();
+                cy.get('tr td:nth-child(2)').next().eq(index).then((price) => {
+                    expect(price.text()).equal('25');
+                });
             };
             
         });
