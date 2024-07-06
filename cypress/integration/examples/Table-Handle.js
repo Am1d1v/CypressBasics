a699f5baf86ca7d301afbfe5e8f92a696b890e81
Am1d1v <dimadiman1adima@gmail.com>
2024-07-06 13:58:44 +0300

    Table handle

--- cypress/integration/examples/Table-Handle.js
+++ cypress/integration/examples/Table-Handle.js
@@ -5,7 +5,15 @@ describe('Table Handle', () => {
     it.only("should validate course and it's price", () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice');
 
-        cy.get('tr td:nth-child(2)')
+        cy.get('tr td:nth-child(2)').each(item => {
+            const text = item.text()
+            
+            if(text.includes('SQL')){
+                // Get "Price" column
+                cy.get('tr td:nth-child(2)').next();
+            };
+            
+        });
     });
 
 });
