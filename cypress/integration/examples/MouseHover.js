85a61208ec6d756ef8c33c8b2dc5341d0d0c6528
Am1d1v <dimadiman1adima@gmail.com>
2024-07-06 16:27:51 +0300

    Mouse Hover. Click on hidden element using force click

--- cypress/integration/examples/MouseHover.js
+++ cypress/integration/examples/MouseHover.js
@@ -2,11 +2,23 @@
 
 describe('Mouse Hover', () => {
 
-    it.only("", () => {
+    it("should hover mouse on input and select 'Top'", () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice');
 
         cy.get('.mouse-hover-content').invoke('show')
         cy.contains('Top').click();
+        
+        // #top should concate with url
+        cy.url().should('include', '#top')
+    });
+
+    it.only("should hover mouse on input and select 'Top' using force click", () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice');
+
+        cy.contains('Top').click({'force': true});
+
+        // #top should concate with url
+        cy.url().should('include', '#top')
     });
 
 });
