b1d62ae5290a2b8d6d34d58d69df086a78b69148
Am1d1v <dimadiman1adima@gmail.com>
2024-07-07 12:58:43 +0300

    Child Window prop();

--- cypress/integration/examples/Child-Window.js
+++ cypress/integration/examples/Child-Window.js
@@ -2,12 +2,24 @@
 
 describe('Handle Child Windows', () => {
 
-    it('Should handle "Open Tab" button', () => {
+    it('Should handle "Open Tab" button by removing attribute', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 
         cy.get('#opentab').invoke('removeAttr', 'target').click();
 
         cy.location('pathname', 'https://www.qaclickacademy.com/');
     });
+
+    it.only('Should handle "Open Tab" button', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+        cy.get('#opentab').then((elem) => {
+            const url = elem.prop('href');
+
+            cy.visit(url);
+            cy.location(url);
+
+        });
+    })
 
 });
