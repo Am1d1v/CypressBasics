0faf81e3b999a84aa371b187957010cd5c3f7a60
Am1d1v <dimadiman1adima@gmail.com>
2024-07-07 13:36:54 +0300

    Frame Navigation

--- cypress/integration/examples/Frames.js
+++ cypress/integration/examples/Frames.js
@@ -1,11 +1,14 @@
 /// <reference types="cypress" />
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe'
 
 describe('Handle Frames', () => {
 
-    it('', () => {
+    it('Navigate in the frame', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 
-        cy.get('#courses-iframe');
+        cy.frameLoaded('#courses-iframe');
+        cy.iframe();
     });
 
 });
