c562cdb40de9019fcdf9ff03156819461d1f76dc
Am1d1v <dimadiman1adima@gmail.com>
2024-07-07 13:49:24 +0300

    Frame Navigation. Select "Mentorship" and click it

--- cypress/integration/examples/Frames.js
+++ cypress/integration/examples/Frames.js
@@ -4,11 +4,11 @@ import 'cypress-iframe'
 
 describe('Handle Frames', () => {
 
-    it('Navigate in the frame', () => {
+    it('Navigate in the frame and select "Mentorship" link', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 
         cy.frameLoaded('#courses-iframe');
-        cy.iframe().find('a[href="mentorship"]')
+        cy.iframe().find('a[href="mentorship"]').eq(0).click();
 
     });
 
