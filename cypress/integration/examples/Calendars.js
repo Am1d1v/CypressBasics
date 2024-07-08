018d4b1555a5dc30dc01e5b6dffecfe09214d3fe
Am1d1v <dimadiman1adima@gmail.com>
2024-07-08 13:49:45 +0300

    Cypress Calendar. Select certain date

--- cypress/integration/examples/Calendars.js
+++ cypress/integration/examples/Calendars.js
@@ -2,10 +2,18 @@
 
 describe('Calendars', () => {
 
-    it('Calendar', () => {
-        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+    it('should choose certain date in calendar', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/offers');
 
-        
+        // Calendar is hidden
+        cy.get('div.react-date-picker').should('have.class', 'react-date-picker--closed');
+
+        // Click on calendar
+        cy.get('.react-date-picker__wrapper').click();
+
+        // After calendar icon was clicked calendar appears
+        cy.get('div.react-date-picker').should('have.class', 'react-date-picker--open');
+
     });
 
     
